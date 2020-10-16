37b19e10039584b81bc99962338734d8f7a0566a
Imran <bg201802497@wmsu.edu.ph>
2020-10-16 14:56:27 +0800

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//The main function
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
